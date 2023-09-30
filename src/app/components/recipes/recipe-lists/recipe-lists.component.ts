5f24d70a80d2f7967a973abca01161392cab6a70
nurg1ssa <nurike2001@mail.ru>
2023-09-30 18:30:34 +0200

    adding content to the recipe lists component

--- src/app/components/recipes/recipe-lists/recipe-lists.component.ts
+++ src/app/components/recipes/recipe-lists/recipe-lists.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-lists',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-lists.component.css']
 })
 export class RecipeListsComponent {
-  recipes = []
+  recipes: Recipes[] = [
+    new Recipes('A test recipe', 'This is a simply test', 'https://img.taste.com.au/hbNtzI2Q/taste/2021/08/clinkers-cake-173208-2.jpg'),
+    new Recipes('A test recipe', 'This is a simply test', 'https://img.taste.com.au/hbNtzI2Q/taste/2021/08/clinkers-cake-173208-2.jpg')
+
+  ]
 }
