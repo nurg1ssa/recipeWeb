3af45a3814bdd766e7e9f8f4cdbe252bd83837d8
nurg1ssa <nurike2001@mail.ru>
2024-01-08 20:58:24 +0100

    Improving Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,28 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 interface AuthResponseData {
-    kind: string
-    idToken: string;
-    email: string;    
-    refreshToken: string
-    expiresIn: string
-    loaclId: string
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  loaclId: string;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  signUp(email:string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCFtBFKgNtHBR2VVRkkwE2Pl9ckA11LD4',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-    }
-    );
+  signUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCFtBFKgNtHBR2VVRkkwE2Pl9ckA11LD4',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occured!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
